647661070700c560eefe12207738d71a9475f9ec
valentincuzin <valentincuzin.pro@ikmail.com>
2025-03-26 15:13:16 +0000

    update

--- Comparative_Study/commu-inner.py
+++ Comparative_Study/commu-inner.py
@@ -35,7 +35,7 @@ hp = {
     'batch_size': 1152,
     'ln': True,
     'lnnn': True,
-    'epochs': 0,
+    'epochs': 100,
     'model': 'puregcn',
     'runs': 10,
     'hiddim': 256,
@@ -60,7 +60,7 @@ hp = {
     	'drop_feature_rate_1': 0.1,
     	'drop_feature_rate_2': 0.0,
     	'tau': 0.4,
-    	'num_epochs': 0,
+    	'num_epochs': 500,
     	'weight_decay': 1e-5,
     	'drop_scheme': 'degree',
     }
@@ -106,13 +106,14 @@ def pretrain_grace_commu(model, data, param):
     for epoch in tqdm(range(1, param['num_epochs'] + 1)):
         model.train()
         optimizer.zero_grad()
-        data_sbm = gen_sbm(data.sizes, data.probs, data.x.device, epoch)
-        edge_index_1 = dropout_adj(data_sbm.edge_index, p=param[f'drop_edge_rate_{1}'])[0]
-        edge_index_2 = dropout_adj(data_sbm.edge_index, p=param[f'drop_edge_rate_{2}'])[0]
-        x_1 = drop_feature(data_sbm.x, param['drop_feature_rate_1'])
-        x_2 = drop_feature(data_sbm.x, param['drop_feature_rate_2'])
-        z1 = model(x_1, edge_index_1)
-        z2 = model(x_2, edge_index_2)
+        data_sbm1 = gen_sbm(data.sizes, data.probs, data.x.device, epoch)
+        data_sbm2 = gen_sbm(data.sizes, data.probs, data.x.device, epoch)
+        # edge_index_1 = dropout_adj(data_sbm.edge_index, p=param[f'drop_edge_rate_{1}'])[0]
+        # edge_index_2 = dropout_adj(data_sbm.edge_index, p=param[f'drop_edge_rate_{2}'])[0]
+        # x_1 = drop_feature(data_sbm.x, param['drop_feature_rate_1'])
+        # x_2 = drop_feature(data_sbm.x, param['drop_feature_rate_2'])
+        z1 = model(data_sbm1.x, data_sbm1.edge_index)
+        z2 = model(data_sbm2.x, data_sbm2.edge_index)
         loss = model.loss(z1, z2)
         loss.backward()
         optimizer.step()
@@ -140,13 +141,13 @@ def pretrain_bgrl_commu(model, data, param):
         optimizer.zero_grad()
         data_sbm = gen_sbm(data.sizes, data.probs, data.x.device)
 
-        data_c1 = data_sbm.clone()
-        data_c2 = data_sbm.clone()
-        data_c1.edge_index = dropout_adj(data_sbm.edge_index, p=param[f'drop_edge_rate_{1}'])[0]
-        data_c2.edge_index = dropout_adj(data_sbm.edge_index, p=param[f'drop_edge_rate_{2}'])[0]
+        data_c1 = gen_sbm(data.sizes, data.probs, data.x.device, epoch)
+        data_c2 = gen_sbm(data.sizes, data.probs, data.x.device, epoch)
+        # data_c1.edge_index = dropout_adj(data_sbm.edge_index, p=param[f'drop_edge_rate_{1}'])[0]
+        # data_c2.edge_index = dropout_adj(data_sbm.edge_index, p=param[f'drop_edge_rate_{2}'])[0]
 
-        data_c1.x = drop_feature(data_sbm.x, param['drop_feature_rate_1'])
-        data_c2.x = drop_feature(data_sbm.x, param['drop_feature_rate_2'])
+        # data_c1.x = drop_feature(data_sbm.x, param['drop_feature_rate_1'])
+        # data_c2.x = drop_feature(data_sbm.x, param['drop_feature_rate_2'])
 
         z1, y2 = model.train_forward(data_c1, data_c2)
         z2, y1 = model.train_forward(data_c2, data_c1)
@@ -172,16 +173,17 @@ def pretrain_csgcl_commu(model, data, param):
     for epoch in tqdm(range(1, param['num_epochs'] + 1)):
         model.train()
         optimizer.zero_grad()
-        data_sbm = gen_sbm(data.sizes, data.probs, data.x.device)
-        g = to_networkx(data_sbm, to_undirected=True)
+        data_sbm1 = gen_sbm(data.sizes, data.probs, data.x.device)
+        data_sbm2 = gen_sbm(data.sizes, data.probs, data.x.device)
+        g = to_networkx(data_sbm1, to_undirected=True)
         communities = community_detection('leiden')(g).communities
-        com = transition(communities, g.number_of_nodes())
+        # com = transition(communities, g.number_of_nodes())
         com_cs, node_cs = community_strength(g, communities)
-        edge_weight = get_edge_weight(data_sbm.edge_index, com, com_cs)
-        edge_index_1 = ced(data_sbm.edge_index, edge_weight, p=param['drop_edge_rate_1'])
-        edge_index_2 = ced(data_sbm.edge_index, edge_weight, p=param['drop_edge_rate_2'])
-        x1 = cav(data_sbm.x, node_cs, param["drop_feature_rate_1"])
-        x2 = cav(data_sbm.x, node_cs, param['drop_feature_rate_2'])
+        # edge_weight = get_edge_weight(data_sbm.edge_index, com, com_cs)
+        # edge_index_1 = ced(data_sbm.edge_index, edge_weight, p=param['drop_edge_rate_1'])
+        # edge_index_2 = ced(data_sbm.edge_index, edge_weight, p=param['drop_edge_rate_2'])
+        # x1 = cav(data_sbm.x, node_cs, param["drop_feature_rate_1"])
+        # x2 = cav(data_sbm.x, node_cs, param['drop_feature_rate_2'])
         z1 = model(x1, edge_index_1)
         z2 = model(x2, edge_index_2)
         loss = model.team_up_loss(z1, z2,
@@ -192,40 +194,6 @@ def pretrain_csgcl_commu(model, data, param):
         if epoch % 100 == 0:
             loss_res.append(round(float(loss), 2))
     print('pretrain loss: ', loss_res, ' s')
-    pre_time = time.time()-t1
-    print(f"pretrain time: {pre_time:.2f} s")
-    return pre_time
-
-
-def pretrain_csgcl_commu2(model, data, param):
-    optimizer = torch.optim.Adam(
-        model.parameters(),
-        lr=param['learning_rate'],
-        weight_decay=param['weight_decay']
-    )
-    t1 = time.time()
-    loss_res = []
-    for epoch in tqdm(range(1, param['num_epochs'] + 1)):
-        model.train()
-        optimizer.zero_grad()
-        data_sbm = gen_sbm(data.sizes, data.probs, data.x.device, epoch)
-        g = to_networkx(data_sbm, to_undirected=True)
-        communities = community_detection('leiden')(g).communities
-        _, node_cs = community_strength(g, communities)
-        edge_index_1 = dropout_adj(data_sbm.edge_index, p=param[f'drop_edge_rate_{1}'])[0]
-        edge_index_2 = dropout_adj(data_sbm.edge_index, p=param[f'drop_edge_rate_{2}'])[0]
-        x_1 = drop_feature(data_sbm.x, param['drop_feature_rate_1'])
-        x_2 = drop_feature(data_sbm.x, param['drop_feature_rate_2'])
-        z1 = model(x_1, edge_index_1)
-        z2 = model(x_2, edge_index_2)
-        loss = model.team_up_loss(z1, z2,
-                                  cs=node_cs,
-                                  current_ep=epoch)
-        loss.backward()
-        optimizer.step()
-        if epoch % 100 == 0:
-            loss_res.append(round(float(loss), 2))
-    print('pretrain loss: ', loss_res)
     pre_time = time.time()-t1
     print(f"pretrain time: {pre_time:.2f} s")
     return pre_time
@@ -259,17 +227,6 @@ def init_model(data, hp):
     predictor = dec.inner_prod().to(device)
     return encoder, predictor
 full_res.append(tr.runs('SBM_CSGCL+inner', init_model, pretrain_csgcl_commu, data_split, evaluator, hp))
-
-def init_model(data, hp):
-    _encoder = enc.ENCODER_GRACE(data.num_features, hp['ct_param']['num_hidden'], nn.Identity()).to(device)
-    encoder = ctmod.CSGCL(_encoder,
-                      hp['ct_param']['num_hidden'],
-                      hp['ct_param']['num_proj_hidden'],
-                      hp['ct_param']['tau']).to(device)
-    predictor = dec.inner_prod().to(device)
-    return encoder, predictor
-full_res.append(tr.runs('SBM_CSGCL2+inner', init_model, pretrain_csgcl_commu2, data_split, evaluator, hp))
-
 
 def init_model(data, hp):
     encoder = enc.ENCODER_NCN(data.num_features, hp['hiddim'], hp['hiddim'], hp['mplayers'],
@@ -375,4 +332,4 @@ full_res.append(tr.runs('BGRL_cs+inner', init_model, pretr.pretrain_bgrl_cs, dat
 
 
 df, tex = ut.full_output(full_res)
-df.to_csv(f'output/SBM_inner_250_0_res.csv', sep=';')
+df.to_csv(f'output/SBM_inner_250_res.csv', sep=';')
